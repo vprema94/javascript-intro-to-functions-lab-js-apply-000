be27c7b6831abf6f477e958ff66bfba2e705c44c
Vanessa Prema <vanessabprema@gmail.com>
2018-11-05 21:12:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  var uppercase = string
+  var lowercase = string
+  var mixedCase = string
+
+  if (uppercase.toUpperCase() === uppercase) // true
+  return "YES INDEED!"
+ 
+  else if (lowercase.toLowerCase() === lowercase) // true
+  return "I can't hear you!"
+
+  else if (mixedCase === "I love you, Grandma.")
+  return "I love you, too."
+}
